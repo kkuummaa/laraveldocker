0f213686ee4fa41c6053106a471eaf4e3820ab3d
熊谷祐人 <kumagaiyuuto@kumagaiyuujinnoMacBook-Air.local>
2022-02-20 12:59:33 +0900

    writed profile model

--- src/laraveldocker/app/Profile.php
+++ src/laraveldocker/app/Profile.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    // $guardedを定義してcreate()でデータを追加する際に、値を代入「させない」フィールドを設定します。（つまりそれ以外は代入が許可されます）
+    protected $guarded = array('id', 'user_id');
+
+    public static $rules = array(
+        'nickname' => 'required',
+    );
 }
